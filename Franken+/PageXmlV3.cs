498124b621ace7b2870e0f25376bc75a56d5ba6b
Sven Lauterbach <SvenLauterbach@googlemail.com>
2015-04-28 10:37:45 +0200

    Modified PageXML to use own ImageExtractor

--- Franken+/PageXmlV3.cs
+++ Franken+/PageXmlV3.cs
@@ -14,12 +14,14 @@ namespace Franken_
 
         public override string ImageExtratorRelPath
         {
-            get { return "ExtractorExporter 1.2\\ExtractorExporter-1-2-50.exe"; }
+            //get { return "ExtractorExporter 1.2\\ExtractorExporter-1-2-50.exe"; }
+            get { return "ImgExtractor\\ImageExtractor.exe"; }
         }
 
         public override string CreateImageExtractorCommandLine(string inputImagePath, string xmlInputPath, string outputFolderPath)
         {
-            string options = string.Format(@"-extract imageSnippets -filter-by type -filter glyph -image ""{0}"" -page-content ""{1}"" -output-folder ""{2}"" -boxes", inputImagePath, xmlInputPath, outputFolderPath);
+            string options = string.Format(@"-i ""{0}"" -x ""{1}"" -o ""{2}""", inputImagePath, xmlInputPath, outputFolderPath);
+            //string options = string.Format(@"-extract imageSnippets -filter-by type -filter glyph -image ""{0}"" -page-content ""{1}"" -output-folder ""{2}"" -boxes", inputImagePath, xmlInputPath, outputFolderPath);
 
             return options;
         }
